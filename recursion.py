4d0d494c33ff7c1b57cfd297642d42bea6b86515
wasim ullah 209 <wasimcs88@gmail.com>
2025-05-10 00:11:41 +0500

    adding the factorial in Recursion

--- recursion.py
+++ recursion.py
@@ -2,12 +2,23 @@
              A programming technique where a function calls itself repeatedly 
 Example:- '''
 
-def show (n):
-    if(n == 0):
-        return 
-    print(n)
-    show (n-1)
+# def show (n):
+#     if(n == 0):
+#         return 
+#     print(n)
+#     show (n-1)
 
-show(5)
-show(50)
-show(10)
+# show(5)
+# show(50)
+# show(10)
+
+# Factorial Recursion
+
+def factorial(n):
+    if(n == 0 or n == 1):
+        return 1
+    return factorial(n-1) * n
+print(factorial(5)) # Ans = 120
+print(factorial(4)) # Ans = 24
+print(factorial(3)) # Ans =6
+print(factorial(8))
